498a421cb9c1877cca804a18da61e51b8b788602
SrishtiSwaraj25 <99004103+SrishtiSwaraj25@users.noreply.github.com>
2022-03-14 13:07:11 +0530

    commit on main

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,11 +1,43 @@
 import React from "react"
-
+import {Link} from "gatsby"
 // export default function Home() {
 //   return <div>Hello world!</div>
 // }
 const Header=()=>
 {
-  return (<div>Header</div>)
+  return (<nav class="navbar navbar-expand-lg navbar-dark bg-dark" aria-label="Fifth navbar example">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Group 10</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample05" aria-controls="navbarsExample05" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarsExample05">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="btn text-light text-decoration-none">
+          <Link class="text-light text-decoration-none" aria-current="page" to="/">Home</Link>
+        </li>
+        <li class="btn text-light text-decoration-none">
+          <Link class="text-light text-decoration-none" to="/about">About</Link>
+        </li>
+        <li class="btn text-light text-decoration-none">
+          <Link class="text-light text-decoration-none" to="/contact">Contact</Link>
+        </li>
+        {/*<li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="dropdown05" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</a>
+          <ul class="dropdown-menu" aria-labelledby="dropdown05">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+</li>*/}
+      </ul>
+      <form>
+        <input class="form-control" type="text" placeholder="Search" aria-label="Search"/>
+      </form>
+    </div>
+  </div>
+</nav>)
 }
 export default Header
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,12 @@
 import React from "react"
+import Hero from "../components/hero/hero"
 import Layout from "../components/layout/layout"
+
 
 const Index = () => {
   return (
     <Layout>
-    <div>
-      <div className=" card shadow m-2 p-2  text-primary">
-        Test <div className="btn btn-warning"> Okay </div>
-      </div>
-    </div>
+    <Hero/>
     </Layout>
   )
 }
